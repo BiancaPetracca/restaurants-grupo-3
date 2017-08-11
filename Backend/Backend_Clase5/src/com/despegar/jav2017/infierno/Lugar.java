0c7929d3d4906651a7fde43912abc33d204c8fe0
Bianca Petracca <bianca.petracca@despegar.com>
2017-08-10 23:07:45 -0300

    Agrego lugar

---
+++ Backend/Backend_Clase5/src/com/despegar/jav2017/infierno/Lugar.java
@@ -0,0 +1,54 @@
+package com.despegar.jav2017.infierno;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Lugar {
+
+    private Set<Alma> almas = new HashSet<>();
+
+    public Set<Alma> getAlmas() {
+        return almas;
+    }
+
+    public void entregar(Alma alma) {
+        almas.remove(alma);
+    }
+
+    public Integer getCantidadDeAlmasJodidas(Demonio demonio) {
+        return almas.stream().filter(demonio::esJodida)
+                .collect(Collectors.toList()).size();
+    }
+
+    public Integer getCantidadDeAlmas() {
+        return almas.size();
+    }
+
+    public void ataqueDe(Demonio demonio) {
+        Integer aumentoDeMaldad = aumentoDeMaldadPara(demonio);
+        almas.forEach(alma ->{
+            demonio.intentarCazar(this, alma);
+                }
+        );
+        tormentoDe(demonio);
+        demonio.ponerseFelizSiCazoMucho();
+
+    }
+
+    public void tormentoDe(Demonio demonio){
+        almas.forEach(demonio::atormentar);
+    }
+
+
+    public Integer aumentoDeMaldadPara(Demonio demonio){
+        Integer almasAtormentadas = getCantidadDeAlmasJodidas(demonio);
+        Integer almasDelLugar = almas.size();
+        return almasAtormentadas + (almasAtormentadas - almasDelLugar) * 2;
+    }
+
+    public Boolean existeUnAlmaAtormentadaHastaLaLocuraPor(Demonio demonio){
+        return almas.stream()
+                .anyMatch(alma -> alma.puedeSerAtormentadaHastaLaLocura(demonio));
+    }
+}
