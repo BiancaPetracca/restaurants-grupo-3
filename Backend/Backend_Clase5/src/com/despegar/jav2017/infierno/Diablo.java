3a39c5d2f9d6731b6da4d3ac70d28b9dfe8f3466
Bianca Petracca <bianca.petracca@despegar.com>
2017-08-11 10:28:54 -0300

    Ahora el diablo no conoce el poder entregado de los demonios

--- Backend/Backend_Clase5/src/com/despegar/jav2017/infierno/Diablo.java
+++ Backend/Backend_Clase5/src/com/despegar/jav2017/infierno/Diablo.java
@@ -4,36 +4,13 @@ import java.util.*;
 
 public class Diablo {
 
-    private Map<Demonio, Double> demoniosACargo = new HashMap<>();
+    private Set<Demonio> demoniosACargo = new HashSet<>();
     private Set<Alma> almas = new HashSet<>();
 
-    public void pedirRendicionDeCuentas(Demonio demonio) {
-        demonio.rendirCuentas(this);
-    }
-
-    public void sumarPoderAdquirido(Demonio demonio, Set<Alma> almasSuministradas){
-        almasSuministradas.forEach(alma ->
-                agregarPoderEntregado(demonio, alma.poder()));
-    }
-
-
-    private void agregarPoderEntregado(Demonio demonio, Double poder) {
-        Double nuevoPoder = demoniosACargo.get(demonio) + poder;
-        demoniosACargo.put(demonio, nuevoPoder);
-    }
-
     private Demonio pequenioDemonio(){
-      Double minimoPoder = Collections.min(demoniosACargo.values());
-      return demoniosACargo.entrySet().stream()
-              .filter(demonio -> demonio.getValue().equals(minimoPoder))
-              .map(Map.Entry::getKey)
-              .findFirst()
+      return demoniosACargo.stream()
+              .max(Comparator.comparingDouble(Demonio::getPoderEntregado))
               .orElse(new NingunDemonio());
-    }
-
-    public void castigar(Demonio demonio) {
-        demonio.disminuirNivelDeMaldadHasta(0.1);
-        demonio.deprimirse();
     }
 
     public void absorber(Set<Alma> almasCazadas) {
