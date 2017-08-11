12b1d3eca7c0ee9f00f317852f7ed2d1740d4263
Bianca Petracca <bianca.petracca@despegar.com>
2017-08-11 10:28:10 -0300

    Se delego el porcentaje de almas

--- Backend/Backend_Clase5/src/com/despegar/jav2017/infierno/Safari.java
+++ Backend/Backend_Clase5/src/com/despegar/jav2017/infierno/Safari.java
@@ -1,12 +1,11 @@
 package com.despegar.jav2017.infierno;
 
-public class Safari extends Mision {
+public class Safari implements Mision {
 
-    public Integer porcentajeDeAlmas;
+    public Double porcentajeDeAlmas;
     public Lugar lugar;
 
     public Boolean puedeSerCumplidaPor(Demonio demonio){
-        return demonio.almasCazables(lugar).size() >
-                porcentajeDeAlmas * lugar.getCantidadDeAlmas();
+        return lugar.porcentajeDeAlmasCazablesPara(demonio) > porcentajeDeAlmas;
     }
 }
