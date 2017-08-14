cf050678f7b9d21b5b5136749416ea57c33b455a
Bianca Petracca <bianca.petracca@despegar.com>
2017-08-14 15:44:04 -0300

    Ahora pequenio demonio tira una excepcion en vez de devolver un objeto nulo

--- Backend/Backend_Clase5/src/com/despegar/jav2017/infierno/Diablo.java
+++ Backend/Backend_Clase5/src/com/despegar/jav2017/infierno/Diablo.java
@@ -1,24 +1,32 @@
 package com.despegar.jav2017.infierno;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Diablo {
 
     private Set<Demonio> demoniosACargo = new HashSet<>();
     private Set<Alma> almas = new HashSet<>();
 
-    private Demonio pequenioDemonio(){
-      return demoniosACargo.stream()
-              .max(Comparator.comparingDouble(Demonio::getPoderEntregado))
-              .orElse(new NingunDemonio());
+    private void chequearExistenciaDeDemonios() {
+        if (demoniosACargo.isEmpty())
+            throw new RuntimeException("El diablo no tiene ningun demonio a cargo");
+    }
+
+    public Demonio pequenioDemonio() {
+        chequearExistenciaDeDemonios();
+        return demoniosACargo.stream()
+                .min(Comparator.comparingDouble(Demonio::getPoderEntregado))
+                .get();
     }
 
     public void absorber(Set<Alma> almasCazadas) {
         almas.addAll(almasCazadas);
     }
 
-    public void asignarMision(Demonio demonio, Mision mision){
-        if(mision.puedeSerCumplidaPor(demonio))
+    public void asignarMision(Demonio demonio, Mision mision) {
+        if (mision.puedeSerCumplidaPor(demonio))
             demonio.notificarQuePuedeCumplirMision();
     }
 }
