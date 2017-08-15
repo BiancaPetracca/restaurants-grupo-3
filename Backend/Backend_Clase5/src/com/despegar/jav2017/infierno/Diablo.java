e7876605ad119b149b06d340680f4d673c759322
Bianca Petracca <bianca.petracca@despegar.com>
2017-08-15 13:23:58 -0300

    Now the devil logs when it doesnt have demons in charge

--- Backend/Backend_Clase5/src/com/despegar/jav2017/infierno/Diablo.java
+++ Backend/Backend_Clase5/src/com/despegar/jav2017/infierno/Diablo.java
@@ -1,24 +1,33 @@
 package com.despegar.jav2017.infierno;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Diablo {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(Diablo.class);
     private Set<Demonio> demoniosACargo = new HashSet<>();
     private Set<Alma> almas = new HashSet<>();
 
     private void chequearExistenciaDeDemonios() {
-        if (demoniosACargo.isEmpty())
-            throw new RuntimeException("El diablo no tiene ningun demonio a cargo");
+        LOGGER.info("Chequeando existencia de demonios...");
+        if (demoniosACargo.isEmpty()) {
+            LOGGER.error("El diablo no tiene ningún demonio a cargo");
+            throw new ElDiabloNoTieneDemoniosACargoException();
+        }
     }
 
     public Demonio pequenioDemonio() {
         chequearExistenciaDeDemonios();
-        return demoniosACargo.stream()
+        Demonio pequenioDemonio = demoniosACargo.stream()
                 .min(Comparator.comparingDouble(Demonio::getPoderEntregado))
                 .get();
+        LOGGER.info("El pequenio demonio del diablo es " + pequenioDemonio.getNombre());
+        return pequenioDemonio;
     }
 
     public void absorber(Set<Alma> almasCazadas) {
@@ -28,6 +37,7 @@ public class Diablo {
     public void asignarMision(Demonio demonio, Mision mision) {
         if (mision.puedeSerCumplidaPor(demonio))
             demonio.notificarQuePuedeCumplirMision();
+        LOGGER.info("El diablo no puede cumplir la mision " + mision.toString() +  "asignada");
     }
 }
 
