5bcc71c8693db3ab467f9fa7706c9df88f73d2e7
Bianca Petracca <bianca.petracca@despegar.com>
2017-08-16 10:43:48 -0300

    Added maven stub project

---
+++ Backend/Backend_Clase7_maven/src/main/java/com/despegar/jav2017/App.java
@@ -0,0 +1,13 @@
+package com.despegar.jav2017;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
