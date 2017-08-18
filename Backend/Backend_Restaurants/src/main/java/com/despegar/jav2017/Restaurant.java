b365c0566314da41a7c45ff4c994214287b78bd3
Bianca Petracca <bianca.petracca@despegar.com>
2017-08-18 10:14:12 -0300

    Added constructor to restaurant

--- Backend/Backend_Restaurants/src/main/java/com/despegar/jav2017/Restaurant.java
+++ Backend/Backend_Restaurants/src/main/java/com/despegar/jav2017/Restaurant.java
@@ -2,6 +2,17 @@ package com.despegar.jav2017;
 
 public class Restaurant {
 
+    public Restaurant(String id, String nombre, Integer telefono, String zona,
+                      Integer recomendaciones, Double costoPorPersona, Boolean activo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.zona = zona;
+        this.recomendaciones = recomendaciones;
+        this.costoPorPersona = costoPorPersona;
+        this.activo = activo;
+    }
+
     private String id;
     private String nombre;
     private Integer telefono;
