f638cb9373e70661e598cb1d9545e164b6c734f0
Bianca Petracca <bianca.petracca@despegar.com>
2017-08-18 11:29:16 -0300

    Deleted other projects

--- Backend/Backend_Clase1/src/com/despegar/jav2017/helloworld/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.despegar.jav2017.helloworld;
-
-public class HelloWorld {
-
-    public static void main(String args[]) {
-
-        System.out.println("Hello World!");
-    }
-}
--- Backend/Backend_Clase4/src/com/despegar/jav2017/contratar/Gerente.java
+++
@@ -1,8 +0,0 @@
-package com.despegar.jav2017.contratar;
-
-public class Gerente implements Categoria {
-    @Override
-    public Double calcularSueldo(Empleado empleado, Empresa empresa) {
-        return empresa.getCantidadDeEmpleados() / 10 * empleado.getSueldoActual();
-    }
-}
